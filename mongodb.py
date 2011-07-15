7f67f2349e6b28d833419e108cc301c16149b859
Sebastien Estienne <sebastien.estienne@gmail.com>
2011-07-15 23:48:07 +0200

    support to monitor many databases

--- mongodb.py
+++ mongodb.py
@@ -1,3 +1,28 @@
+#
+# Plugin to check MongoDB
+#
+
+# Example configuration
+# <LoadPlugin python>
+#    Globals true
+# </LoadPlugin>
+#
+# <Plugin python>
+#   ModulePath "/usr/share/collectd/plugins/python"
+#   LogTraces true
+#   Interactive false
+#
+#   Import "mongodb"
+#   <Module mongodb>
+#     Host "127.0.0.1"
+#     Port 27017
+#     User "user"
+#     Password "password"
+#     Database "admin" "db-prod" "db-dev"
+#   </Module>
+# </Plugin>
+#
+
 import collectd
 from pymongo import Connection
 
@@ -8,14 +33,18 @@ class MongoDB(object):
         self.plugin_name = "mongo"
         self.mongo_host = "127.0.0.1"
         self.mongo_port = 27017
-        self.mongo_db = "admin"
+        self.mongo_db = ["admin", ]
         self.mongo_user = None
         self.mongo_password = None
 
-    def submit(self, type, instance, value):
+    def submit(self, type, instance, value, db=None):
+        if db:
+            plugin_instance = '%s-%s' % (self.mongo_port, db)
+        else:
+            plugin_instance = str(self.mongo_port)
         v = collectd.Values()
         v.plugin = self.plugin_name
-        v.plugin_instance = str(self.mongo_port)
+        v.plugin_instance = plugin_instance
         v.type = type
         v.type_instance = instance
         v.values = [value, ]
@@ -23,7 +52,7 @@ class MongoDB(object):
 
     def do_server_status(self):
         con = Connection(host=self.mongo_host, port=self.mongo_port)
-        db = con[self.mongo_db]
+        db = con[self.mongo_db[0]]
         if self.mongo_user and self.mongo_password:
             db.authenticate(self.mongo_user, self.mongo_password)
         server_status = db.command('serverStatus')
@@ -47,6 +76,23 @@ class MongoDB(object):
         misses = server_status['indexCounters']['btree']['misses']
         self.submit('cache_ratio', 'cache_misses', accesses / float(misses) if misses else 0)
 
+        for mongo_db in self.mongo_db:
+            db = con[mongo_db]
+            if self.mongo_user and self.mongo_password:
+                db.authenticate(self.mongo_user, self.mongo_password)
+            db_stats = db.command('dbstats')
+
+            # stats counts
+            self.submit('counter', 'object_count', db_stats['objects'], mongo_db)
+            self.submit('counter', 'collections', db_stats['collections'], mongo_db)
+            self.submit('counter', 'num_extents', db_stats['numExtents'], mongo_db)
+            self.submit('counter', 'indexes', db_stats['indexes'], mongo_db)
+
+            # stats sizes
+            self.submit('file_size', 'storage', db_stats['storageSize'], mongo_db)
+            self.submit('file_size', 'index', db_stats['indexSize'], mongo_db)
+            self.submit('file_size', 'data', db_stats['dataSize'], mongo_db)
+
         con.disconnect()
 
     def config(self, obj):
@@ -60,7 +106,7 @@ class MongoDB(object):
             elif node.key == 'Password':
                 self.mongo_password = node.values[0]
             elif node.key == 'Database':
-                self.mongo_db = node.values[0]
+                self.mongo_db = node.values
             else:
                 collectd.warning("mongodb plugin: Unkown configuration key %s" % node.key)
 
